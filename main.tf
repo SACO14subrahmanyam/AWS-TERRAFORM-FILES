35e68b04820159c2af0cededc4ff7d5078bd9053
Penujuli Subrahmanyam <96356756+SACO14subrahmanyam@users.noreply.github.com>
2024-11-12 23:57:04 +0530

    THIS IS THE FOR CREATING THE EC2 INSTANCE

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+  access_key = "YOUR ACCESS KEY "
+  secret_key = "YOUR SECRET KEY"
+}
+resource "tls_private_key" "rsa-4096-example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+variable "key_name" {}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa-4096-example.public_key_openssh
+}
+resource "local_file" "private_key" {
+    content = tls_private_key.rsa-4096-example.private_key_pem
+    filename = var.key_name
+}
+resource "aws_instance" "public_instance" {
+  ami           = "ami-063d43db0594b521b"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.key_pair.key_name
+  tags = {
+    Name = "HelloWorld"
+  }
+}
